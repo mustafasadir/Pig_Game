8bc544a56791286e51c246b6ddff96ea9a567795
mustafasadir <mustafasadir@gmail.com>
2023-02-24 15:26:55 +0100

    finally!

--- App.py
+++ App.py
@@ -1,12 +1,6 @@
 print("Welcome to out project Kristoffer & Mustafa")
 print("I hope this goes to the master branch!")
-
 print(  " Here is a new line")
-
-
 print("H")
-<<<<<<< HEAD
 print("Here is Kristoffers first commit") #Commit
-=======
 print("this is the commit with vs code")
->>>>>>> 2300e51a87c1d28600289198037976f6b8084e5e
