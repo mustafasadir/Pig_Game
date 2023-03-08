265439a2cf7a16b41e32027c50ec210d4f13dc86
KristofferLarsson5 <kristoffer.larsson01@gmail.com>
2023-03-08 21:36:57 +0100

    Fixed GameState before merge

--- GameState.py
+++ GameState.py
@@ -1,12 +1,12 @@
 from ast import Dict
-from Game import Difficulty
+from Difficulty import Difficulty
 
 
 class GameState:
     def __init__(self):
         self.difficulty = None #Type ENUM
         self.isCheatMode = False
-        #self.highScoreList : Dict[Player,Score]{}
+        self.highScoreList = {}
         
     def getState(self):
         return self
