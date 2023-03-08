1876613c999eec778a8a0db83e7bacbc83cd0372
mustafasadir <mustafasadir@gmail.com>
2023-03-08 19:55:53 +0100

    sync branch

--- Player.py
+++ Player.py
@@ -3,9 +3,9 @@ from Score import Score
 
 class Player:
 
-    def __init__(self, name : str ):
-        self.__name = name
-        self.score = Score().getCurrentScore  # Creates a Score object for every Player object
+    def __init__(self, name : str,)
+        self.score = Score().getCurrentScore()  # Creates a Score object for every Player object
+        self.name = name
 
     def _getName(self):
         return self.__name
